4eda35ddfc19b1956142f9c5b3838e533e3c7e52
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-03-24 16:37:01 +0200

    [W2] Task 4

--- laskarit/viikko2/unicafe/src/kassapaate.py
+++ laskarit/viikko2/unicafe/src/kassapaate.py
@@ -1,34 +1,5 @@
-class Maksukortti:
-    def __init__(self, saldo):
-        self.saldo = saldo
-
-    def __str__(self):
-        return f'Kortilla on rahaa {self.saldo_euroina():.2f} euroa'
-
-    def saldo_euroina(self):
-        return self.saldo / 100
-
-    def lataa_rahaa(self, saldo):
-        self.saldo = min(self.saldo + saldo, 150 * 100)
-
-    def ota_rahaa(self, saldo):
-        if saldo <= self.saldo:
-            self.saldo -= saldo
-            return True
-        return False
-
-    def syo_maukkaasti(self):
-        if 400 <= self.saldo:
-            self.saldo -= 400
-
-    def syo_edullisesti(self):
-        if 250 <= self.saldo:
-            self.saldo -= 250
-
-
 class Kassapaate:
     def __init__(self):
-        # kassan rahamäärä on senteissä
         self.kassassa_rahaa = 100000
         self.edulliset = 0
         self.maukkaat = 0
