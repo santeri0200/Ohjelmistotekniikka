bf5b5ef89b9cb186d0bd0fd9fbd734ab26244058
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-03-24 16:15:43 +0200

    [W2] Task 2

--- laskarit/viikko2/src/maksukortti.py
+++ laskarit/viikko2/src/maksukortti.py
@@ -1,2 +1,20 @@
-class Maksukortti():
-    
+class Maksukortti:
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+    def __str__(self):
+        return f'Kortilla on rahaa {self.saldo_euroina():.2f} euroa'
+
+    def saldo_euroina(self):
+        return self.saldo / 100
+
+    def lataa_rahaa(self, saldo):
+        self.saldo = min(self.saldo + saldo, 150 * 100)
+
+    def syo_maukkaasti(self):
+        if 400 <= self.saldo:
+            self.saldo -= 400
+
+    def syo_edullisesti(self):
+        if 250 <= self.saldo:
+            self.saldo -= 250
---
+++ laskarit/viikko2/src/tests/maksukortti_test.py
@@ -0,0 +1,48 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.kortti = Maksukortti(1000)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        # alustetaan maksukortti, jossa on 10 euroa (1000 senttiä)
+        kortti = Maksukortti(1000)
+
+        self.assertEqual(str(kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(str(kortti), "Kortilla on rahaa 7.50 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein_2(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+
+        # varmistetaan että saldoa jäljellä 7.5 euroa eli 750 senttiä
+        self.assertEqual(kortti.saldo_euroina(), 7.5)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 6.0)
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.kortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 35.0)
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.kortti.lataa_rahaa(20000)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+
